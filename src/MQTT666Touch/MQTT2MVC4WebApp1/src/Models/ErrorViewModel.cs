2b961a5cbd75694a6c07597028bbfcb8f51c0c3c
Cockroach888 <Cockroach888@outlook.com>
2022-03-31 15:27:40 +0800

    feat: 提交MQTT各实践示例程序库。

---
+++ src/MQTT666Touch/MQTT2MVC4WebApp1/src/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MQTT2MVC4WebApp1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
