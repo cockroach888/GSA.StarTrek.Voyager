a26bd999c35cfc429718275fb2e0ea9acfa70be4
Cockroach888 <Cockroach888@outlook.com>
2023-11-18 18:21:47 +0800

    docs: 优化远程仓库应用，因不可抗力问题，暂时屏蔽azure和bitbucket的冗余性远程仓储。

--- AutoPush.cmd
+++ AutoPush.cmd
@@ -5,20 +5,20 @@ set basedir="%~dp0"
 set basedir
 
 
-@echo\&echo 推送到 Gitee 远程仓库
-git push gitee
-
 @echo\&echo 推送到 GitHub 远程仓库
-git push github
+git push github --all
 
-@echo\&echo 推送到 Bitbucket 远程仓库
-git push bitbucket
+::@echo\&echo 推送到 Bitbucket 远程仓库
+::git push bitbucket --all
 
-@echo\&echo 推送到 Azure 远程仓库
-git push azure
+::@echo\&echo 推送到 Azure 远程仓库
+::git push azure --all
+
+@echo\&echo 推送到 Gitee 远程仓库
+git push gitee --all
 
 ::@echo\&echo 推送到 Origin 远程仓库
-::git push origin
+::git push origin --all
 
 @echo\&echo 查看本地仓库状态
 git status
